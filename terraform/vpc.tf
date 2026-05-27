254b3f9ae27b5db86390bae5736ecb5a14fb9430
arslan-ashraf <arslanash@gmail.com>
2026-05-27 15:52:28 -0400

    changes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -39,14 +39,6 @@ resource "aws_route_table" "rtb_public_subnets" {
 }
 
 resource "aws_route_table_association" "rtb_associations_public_subnets" {
-  # convert list to objects
-  # for_each       = { for item in var.route_table_associations_config_list : item.subnet => item }
-  # the map of objects now looks like:
-  # { 
-  #   AZ_a_public_subnet_1 = { subnet = "AZ_a_public_subnet_1" }, 
-  #   AZ_b_public_subnet_1 = { subnet = "AZ_b_public_subnet_1" }
-  # }
-
   count          = length(var.route_table_associations_subnet_list)
 
   subnet_id      = aws_subnet.subnets_in_main_vpc[
