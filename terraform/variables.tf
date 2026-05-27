0c7ee0dd4e1f69508cca3d74a9d28600bd3a5474
arslan-ashraf <arslanash@gmail.com>
2026-05-27 13:24:41 -0400

    changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,4 +10,18 @@ variable "subnet_config_map" {
 
     error_message = "CIDR Error: At least one of the privded CIDR blocks is invalid."
   }
-} 
+}
+
+
+variable "security_group_config_map" {
+  type = map(object({
+    name = string
+  }))
+
+  validation {
+    condition = alltrue([
+      for security_group_config in value(var.security_group_config_map) : 
+      contains(["public", "private"], security_group_config.name)
+    ])
+  }
+}
