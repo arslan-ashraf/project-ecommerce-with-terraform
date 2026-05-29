63965d9fd6c865d9698ba2df1cf6274c783071ad
arslan-ashraf <arslanash@gmail.com>
2026-05-28 21:07:30 -0400

    changes

--- terraform/s3-bucket.tf
+++ terraform/s3-bucket.tf
@@ -1,9 +1,21 @@
-resource "aws_s3_bucket" "static_files_s3_bucket" {
-  bucket = "static-files-bucket-3l5ka8nb5"
+resource "random_id" "random_bucket_suffix" {
+  byte_length = 5
+}
+
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = "static-website-bucket-${random_id.random_bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_with_public_access" {
+  bucket                  = aws_s3_bucket.static_website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
-  bucket = aws_s3_bucket.static_files_s3_bucket.id
+  bucket = aws_s3_bucket.static_website_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -13,7 +25,7 @@ resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.static_files_s3_bucket.arn}/*"
+        Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
       }
     ]
   })
@@ -21,7 +33,7 @@ resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
 }
 
 resource "aws_s3_bucket_website_configuration" "static_website_config" {
-  bucket = aws_s3_bucket.static_files_s3_bucket.id
+  bucket = aws_s3_bucket.static_website_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -34,7 +46,7 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
 }
 
 resource "aws_s3_object" "index_html_page" {
-  bucket       = aws_s3_bucket.static_files_s3_bucket.id
+  bucket       = aws_s3_bucket.static_website_bucket.id
   key          = "index.html"
   source       = "index.html"
   content_type = "text/html"
@@ -42,7 +54,7 @@ resource "aws_s3_object" "index_html_page" {
 }
 
 resource "aws_s3_object" "error_html_page" {
-  bucket       = aws_s3_bucket.static_files_s3_bucket.id
+  bucket       = aws_s3_bucket.static_website_bucket.id
   key          = "error.html"
   source       = "error.html"
   content_type = "text/html"
